6e20c87142d67596b02a9d7d9b9d155d9c3199ca
AvinashSubhash <avinashsubhash19@gmail.com>
2020-12-11 16:42:15 +0530

    Initial commit with first program

---
+++ creating_excel.py
@@ -0,0 +1,25 @@
+import random
+import openpyxl
+print("Enter the title of the names column to be made:")
+name = input()
+name_list = []
+
+wb = openpyxl.load_workbook("input_data.xlsx")
+
+sheet = wb.active
+
+for i in range(7,84):
+    name_list.append(sheet['C'+str(i)].value)
+
+print(name_list)
+
+wb_out = openpyxl.Workbook()
+
+sheet_out = wb_out.active
+
+sheet_out['A1'].value = name
+
+for i in range(1,78):
+    sheet_out['A'+str(i)].value = sheet['C'+str(i+6)].value
+
+wb_out.save("output_data.xlsx")
